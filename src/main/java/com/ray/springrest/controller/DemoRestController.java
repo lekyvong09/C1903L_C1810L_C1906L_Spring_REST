e39a9ab19632fe19c088f98b1d26c8bbcff16937
lekyvong09 <lekyvong09@gmail.com>
2021-10-29 20:23:21 +0700

    First Spring REST controller

---
+++ src/main/java/com/ray/springrest/controller/DemoRestController.java
@@ -0,0 +1,15 @@
+package com.ray.springrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoRestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
