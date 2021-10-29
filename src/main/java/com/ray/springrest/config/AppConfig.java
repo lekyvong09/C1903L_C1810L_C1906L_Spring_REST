e55e09efc5be4ee2737d1c5462cdda13ddcdd2ea
lekyvong09 <lekyvong09@gmail.com>
2021-10-29 20:19:25 +0700

    Initial commit

---
+++ src/main/java/com/ray/springrest/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.ray.springrest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.ray.springrest")
+public class AppConfig {
+}
